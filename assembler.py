97ad55815a67f6a7224c361e8e4ac64c5a40f13f
dma-neves <david.m.neves@live.com>
2021-02-13 23:51:19 +0000

    Rewrote the assembler

--- assembler.py
+++ assembler.py
@@ -1,15 +1,49 @@
 import sys
 
-def printError(lc):
+instValue = {
+    'addRARB' : 0,
+    'addRARC' : 1,
+    'addRA$X' : 2,
+    'subRARB' : 3,
+    'subRARC' : 4,
+    'subRA$X' : 5,
+    'incRA' : 6,
+    'decRA' : 7,
+    'negRA' : 8,
+    'notRA' : 9,
+    'andRARB' : 10,
+    'andRARC' : 11,
+    'orRARB' : 12,
+    'orRARC': 13,
 
-    print("syntax error: line " + str(lc))
-    quit()
+    'lodadrRA' : 14,
+    'strRAadr' : 15,
+    'lodadrRB' : 16,
+    'strRBadr' : 17,
+    'lodadrRC' : 18,
+    'strRBadr' : 19,
+    'lodACRRA' : 20,
+    'strACRadr' : 21,
+    'lod$XRA' : 22,
+
+    'jmpadr' : 23,
+    'jmpzadr' : 24,
+    'jmpnadr' : 25,
+    'jmpoadr' : 26,
+    'hlt' : 27
+    }
 
 def intTo8bitStr(n):
+
     bitStr = "{0:b}".format(n)
     missingZeros = 8 - len(bitStr)
 
     return '0'*missingZeros + bitStr
+
+def printError(lc):
+
+    print("syntax error: line " + str(lc))
+    quit()
 
 args = sys.argv
 
@@ -28,225 +62,56 @@ else:
     f = open(codeFile,'r')
     
     for line in f:
-
         lc += 1
-        words = line.split()
-        if len(words) != 0 and words[0][0] != '#' and words[0][0] != ';':
+        tokens = line.split()
 
-            data = ""
-            inst = ""
+        # Check if the line isnt empty or a comment line
+        if len(tokens) != 0 and tokens[0][0] != "#" and tokens[0][0] != ";":
 
-            if words[0] == "add":
-                if len(words) != 3:
-                    printError(lc)
-                
-                if words[1] == "RA" and words[2] == "RB":
-                    data = "00000000"
-                    inst = "00000000"
-                
-                elif words[1] == "RA" and words[2] == "RC":
-                    data = "00000000"
-                    inst = "00000001"
-
-                elif words[1] == "RA" and words[3][0] == '$':
-                    data = intTo8bitStr( int( words[3][1:] ) )
-                    inst = "00000010"
-
+            # Check if line is label for jump instruction
+            if tokens[-1][-1] == ':':
+                if len(tokens) == 1:
+                    label = tokens[0][:-1]
+                    # Memory value for the jump instruction using this label will be the current instruction - 1
+                    # sinse after executing the jump, the IC is incremented
+                    labels[label] = ic-1
                 else:
                     printError(lc)
-
-            elif words[0] == "sub":
-                if len(words) != 3:
-                    printError(lc)
-                
-                if words[1] == "RA" and words[2] == "RB":
-                    data = "00000000"
-                    inst = "00000011"
-                
-                elif words[1] == "RA" and words[2] == "RC":
-                    data = "00000000"
-                    inst = "00000100"
-
-                elif words[1] == "RA" and words[3][0] == '$':
-                    data = intTo8bitStr( int( words[3][1:] ) )
-                    inst = "00000101"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "inc":
-                if len(words) != 2:
-                    printError(lc)
-
-                if words[1] == "RA":
-                    data = "00000000"
-                    inst = "00000110"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "dec":
-                if len(words) != 2:
-                    printError(lc)
-
-                if words[1] == "RA":
-                    data = "00000000"
-                    inst = "00000111"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "neg":
-                if len(words) != 2:
-                    printError(lc)
-
-                if words[1] == "RA":
-                    data = "00000000"
-                    inst = "00001000"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "not":
-                if len(words) != 2:
-                    printError(lc)
-
-                if words[1] == "RA":
-                    data = "00000000"
-                    inst = "00001001"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "and":
-                if len(words) != 3:
-                    printError(lc)
-                
-                if words[1] == "RA" and words[2] == "RB":
-                    data = "00000000"
-                    inst = "00001010"
-                
-                elif words[1] == "RA" and words[2] == "RC":
-                    data = "00000000"
-                    inst = "00001011"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "or":
-                if len(words) != 3:
-                    printError(lc)
-                
-                if words[1] == "RA" and words[2] == "RB":
-                    data = "00000000"
-                    inst = "00001100"
-                
-                elif words[1] == "RA" and words[2] == "RC":
-                    data = "00000000"
-                    inst = "00001101"
-
-                else:
-                    printError(lc)
-
-            elif words[0] == "lod":
-                if len(words) != 3:
-                    printError(lc)
-
-                if words[1] == "ACR" and words[2] == "RA":
-                    data = "00000000"
-                    inst = "00010100"
-
-                elif words[1][0] == '$' and words[2] == "RA":
-                    data = intTo8bitStr( int( words[1][1:] ) )
-                    inst = "00010110"
-                
-                else:
-                    data = intTo8bitStr( int( words[1] ) )
-
-                    if words[2] == "RA":
-                        inst = "00001110"
-
-                    elif words[2] == "RB":
-                        inst = "00010000"
-
-                    elif words[2] == "RC":
-                        inst = "00010010"
-
-                    else:
-                        printError(lc)
-
-            elif words[0] == "str":
-                if len(words) != 3:
-                    printError(lc)
-
-                data = intTo8bitStr( int( words[2] ) )
-
-                if words[1] == "ACR":
-                    inst = "00010101"
-
-                elif words[1] == "RA":
-                    inst = "00001111"
-
-                elif words[1] == "RB":
-                    inst = "00010001"
-
-                elif words[1] == "RC":
-                    inst = "00010011"
-                
-                else:
-                    printError(lc)
-
-            elif words[0] == "jmp":
-                if len(words) != 2:
-                    printError(lc)
-
-                data = labels[words[1]]
-                inst = "00010111"
-
-            elif words[0] == "jmpz":
-                if len(words) != 2:
-                    printError(lc)
-
-                data = labels[words[1]] 
-                inst = "00011000"
-
-            elif words[0] == "jmpn":
-                if len(words) != 2:
-                    printError(lc)
-
-                data = labels[words[1]]
-                inst = "00011001"
-                
-            elif words[0] == "jmpo":
-                if len(words) != 2:
-                    printError(lc)
-
-                data = labels[words[1]]
-                inst = "00011010"
-
-            elif words[0] == "hlt":
-                data = "00000000"
-                inst = "00011011"
-
-            elif words[0][-1] == ':':
-                label = words[0][:-1]
-                labels[label] = intTo8bitStr(ic-1)
-
             else:
-                printError(lc)
-            
-            if words[0][-1] != ':':
-                instructions.append(data + inst)
                 ic += 2
+                instStr = ""
+                data = 0
+                
+                for t in tokens:
+                    
+                    token_wd = t.replace('$', '') # Token without dollar
 
-            if ic >= 256:
-                print("error: program to big (maximum of 128 instructions)")
-                quit()
+                    # If token is a numeric valuel, attribute it to the instruction data
+                    if token_wd.isnumeric():
+                        data = int(token_wd)
+                        if '$' in t:
+                            instStr += '$X'
+                        else:
+                            instStr += 'adr'
+                    # If token is a label, attribute the correct memory address to the instruction data
+                    elif t in labels:
+                        data = labels[t]
+                        instStr += 'adr'
+                    else:
+                        instStr += t
+
+                # Check for invalid instruction
+                if not (instStr in instValue):
+                    printError(lc)
+
+                inst = instValue[instStr]
+                b = intTo8bitStr(data) + intTo8bitStr(inst)
+                # Add instruction to the list
+                instructions.append(b)
 
     f.close()
-
+    # Write instructions to the binary file
     f = open(codeFile.replace(".s", ".binary"), 'w')
-
     for i in instructions:
         f.write(i)
         f.write("\n")
